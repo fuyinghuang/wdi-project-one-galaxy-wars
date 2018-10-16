4378cb2d0b063de8622bdb3c5eaf1008f372cd10
huangfuin11 <huangfuin1101@yahoo.com.tw>
2018-10-16 10:45:27 +0100

    circle light each time

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const purple = document.getElementById('purple');
 const circle = document.getElementsByClassName('circle');
 
 let round = 1;
-let block = 0;
+let circleIndex = 0;
 const computerSequence = [];
 // let computerSequence = ['grey', 'grey'];
 console.log(computerSequence);
@@ -16,6 +16,7 @@ let playerSequence = [];
 function startButton() {
   console.log('startButton');
   randomCircle();
+  computerGenerate();
   lightCircle();
   // computerSequence.forEach(color => lightCircle(color));
   // lightCircle('blue');
@@ -23,9 +24,9 @@ function startButton() {
 
 const clickedOn = function() {
   console.log('clicked');
-  console.log(this.id+' '+block);
+  console.log(this.id+' '+circleIndex);
   // alert(this.id);
-  // alert(this.id+' '+block);
+  // alert(this.id+' '+circleIndex);
   playerSequence.push(this.id);
   console.log(playerSequence);
   compareCircle();
@@ -40,57 +41,71 @@ for (let i = 0; i < circle.length; i++) {
 
 
 // generate random number
-function randomCircle(round) {
+function randomCircle() {
   console.log('randomCircle started');
-  block = Math.floor(Math.random() * 4);
-  console.log(block);
+  circleIndex = Math.floor(Math.random() * 4);
+  console.log(circleIndex);
 }
 
 
-//computer sequence
+//
 // function lightCircle(color) {
 function lightCircle() {
   console.log('lightCircle Started');
-  // if (color === 'blue') {
-  if (block === 0) {
-    blue.style.backgroundColor ='#8cbed647';
-    console.log('blue');
-    setTimeout(function(){
-      blue.style.backgroundColor ='#8CBED6';
-    }, 1000);
+  computerSequence.forEach((color, index) => {
+    const time = (index * 1000) + 1500;
+    console.log(time);
+    setTimeout(() => {
+      if(color === 'blue') {
+        blue.style.backgroundColor ='#8cbed647';
+        console.log('blue', blue);
+        setTimeout(() => {
+          blue.style.backgroundColor ='#8CBED6';
+        }, 500);
+      }
+      if(color === 'grey') {
+        grey.style.backgroundColor ='#80808061';
+        console.log('grey', grey);
+        setTimeout(() => {
+          grey.style.backgroundColor ='#808080';
+        }, 500);
+
+      }
+      if(color === 'pink') {
+        pink.style.backgroundColor ='#de707561';
+        console.log('pink', pink);
+        setTimeout(() => {
+          pink.style.backgroundColor ='#de7075';
+        }, 500);
+      }
+      if(color === 'purple') {
+        purple.style.backgroundColor ='#cca2ce59';
+        console.log('purple', purple);
+        setTimeout(() => {
+          purple.style.backgroundColor ='#cca2ce';
+        }, 500);
+      }
+    }, time);
+  });
+}
+
+// computer sequence
+function computerGenerate(){
+  if (circleIndex === 0){
     computerSequence.push('blue');
   }
-  // if (color === 'grey') {
-  if (block === 1) {
-    grey.style.backgroundColor ='#80808061';
-    console.log('grey');
-    setTimeout(function(){
-      grey.style.backgroundColor ='#808080';
-    }, 1000);
+  if (circleIndex === 1){
     computerSequence.push('grey');
   }
-  // if (color === 'pink') {
-  if(block === 2) {
-    pink.style.backgroundColor ='#de707561';
-    console.log('pink');
-    setTimeout(function(){
-      pink.style.backgroundColor ='#de7075';
-    }, 1000);
+  if (circleIndex === 2){
     computerSequence.push('pink');
   }
-  // if (color === 'purple') {
-  if (block === 3) {
-    purple.style.backgroundColor ='#cca2ce59';
-    console.log('purple');
-    setTimeout(function(){
-      purple.style.backgroundColor ='#cca2ce';
-    }, 1000);
+  if (circleIndex === 3){
     computerSequence.push('purple');
   }
-  //console.log(computerSequence);
-  //console.log('lightblock ended');
-  // compareCircle();
+  console.log(computerSequence);
 }
+
 
 
 // setInterval(function() {
@@ -109,6 +124,7 @@ function compareCircle(){
     round++;
     console.log('round: ' + round);
     randomCircle();
+    computerGenerate();
     lightCircle();
   } else if (playerSequence.length === computerSequence.length){
     playerSequence = [];
@@ -124,21 +140,16 @@ function restGame(){
   startButton();
 }
 
-// for (let i = 0; i === round; i++) {
-//   randomCircle();
-//   lightCircle();
-//   // startButton();
-//
-// }
-// //   // playerClick();
-// //   // for (var j = 0; j === computerSequence.length; j++) {
-// //
-// //   // }
-// //
-// round ++;
-// // if the same, secound round
+
+
 //
 // setInterval(function() {
 // // const gameId = setInterval(function() {
 //   startButton();
 // }, 3000);
+// playerClick();
+// //   // for (var j = 0; j === computerSequence.length; j++) {
+// //
+// //   // }
+//  round ++;
+//
